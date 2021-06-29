67bb43c88ff9432e73aafaff19faa889f4e6d261
yulia633 <ignat633yulia@yandex.ru>
2021-06-29 16:48:44 +0300

    new heroku cleardb config

---
+++ database/db.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.34, for Linux (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version       5.7.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sku` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `price` float NOT NULL,
+  `type` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (2,90,'product1',15,'game'),(3,92,'product1',14,'hit'),(22,76,'product4',890,'hit'),(23,77,'product4',490,'hit'),(24,78,'product1',15,'game'),(26,101,'product6',90,'game'),(27,102,'product1',15,'game');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-29 12:20:01
--- src/App/Configs.php
+++ src/App/Configs.php
@@ -1,13 +1,32 @@
 <?php
 
-$container->set('database', function () {
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+$container->set('database', function () use ($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) {
     return (object) [
         'default' => [
-            "DATABASE_HOST" => 'mysql',
-            "DATABASE_NAME" => 'mydb',
-            "DATABASE_USER" => 'root',
-            "DATABASE_PASSWORD" => 'mysql',
+            "DATABASE_HOST" => $cleardb_server,
+            "DATABASE_NAME" => $cleardb_username,
+            "DATABASE_USER" => $cleardb_password,
+            "DATABASE_PASSWORD" => $cleardb_db,
             "DATABASE_CHAR" => 'utf8'
         ]
     ];
 });
+
+// $container->set('database', function () {
+//     return (object) [
+//         'default' => [
+//             "DATABASE_HOST" => 'mysql',
+//             "DATABASE_NAME" => 'mydb',
+//             "DATABASE_USER" => 'root',
+//             "DATABASE_PASSWORD" => 'mysql',
+//             "DATABASE_CHAR" => 'utf8'
+//         ]
+//     ];
+// });
