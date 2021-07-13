d1e6b0664b444ee2642db5de1728a05c862dbea0
yulia633 <ignat633yulia@yandex.ru>
2021-07-13 09:29:21 +0300

    fix configs

--- src/App/Configs.php
+++ src/App/Configs.php
@@ -1,32 +1,23 @@
 <?php
 
-//Get Heroku ClearDB connection information
-$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server   = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db       = substr($cleardb_url["path"],1);
+$container->set('database', function () {
+    if ($url = getenv('DATABASE_URL')) {
+        $urlParts = parse_url($url);
 
-$container->set('database', function () use ($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) {
-    return (object) [
-        'default' => [
-            "DATABASE_HOST" => $cleardb_server,
-            "DATABASE_NAME" => $cleardb_username,
-            "DATABASE_USER" => $cleardb_password,
-            "DATABASE_PASSWORD" => $cleardb_db,
+        return [
+            "DATABASE_HOST" => $urlParts['host'],
+            "DATABASE_NAME" => substr($urlParts['path'], 1),
+            "DATABASE_USER" => $urlParts['user'],
+            "DATABASE_PASSWORD" => $urlParts['pass'],
             "DATABASE_CHAR" => 'utf8'
-        ]
+        ];
+    }
+
+    return [
+        "DATABASE_HOST" => getenv('DATABASE_HOST') ?? 'mysql',
+        "DATABASE_NAME" =>  getenv('DATABASE_NAME') ?? 'mydb',
+        "DATABASE_USER" =>  getenv('DATABASE_USER') ?? 'root',
+        "DATABASE_PASSWORD" =>  getenv('DATABASE_PASSWORD') ?? 'mysql',
+        "DATABASE_CHAR" => 'utf8'
     ];
 });
-
-// $container->set('database', function () {
-//     return (object) [
-//         'default' => [
-//             "DATABASE_HOST" => 'mysql',
-//             "DATABASE_NAME" => 'mydb',
-//             "DATABASE_USER" => 'root',
-//             "DATABASE_PASSWORD" => 'mysql',
-//             "DATABASE_CHAR" => 'utf8'
-//         ]
-//     ];
-// });
--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -4,7 +4,7 @@ use Psr\Container\ContainerInterface;
 
 $container->set('db', function (ContainerInterface $c) {
 
-    $config = $c->get('database')->default;
+    $config = $c->get('database');
 
     $DB_HOST = $config['DATABASE_HOST'];
     $DB_NAME = $config['DATABASE_NAME'];
@@ -12,11 +12,11 @@ $container->set('db', function (ContainerInterface $c) {
     $DB_PASSWORD = $config['DATABASE_PASSWORD'];
     $DB_CHARSET = $config['DATABASE_CHAR'];
 
-    $DSN = "mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME . ";charset=" . $DB_CHARSET;
+    $DSN = "mysql:host={$DB_HOST};dbname={$DB_NAME};charset={$DB_CHARSET}";
 
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ];
 
     return new PDO($DSN, $DB_USER, $DB_PASSWORD, $options);
